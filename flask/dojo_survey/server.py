b0b535a8ea33ea251db1aad476c37daf82838c84
KYU102 <kevinyu25@gmail.com>
2022-04-16 15:25:48 -0700

    created a survey with validations

--- flask/dojo_survey/server.py
+++ flask/dojo_survey/server.py
@@ -1,25 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe'
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    print("Got Post Info")
-    print(request.form)
-    session['fname'] = request.form['fname']
-    session['lang']= request.form['lang']
-    session['loc'] = request.form['loc']
-    session['com'] = request.form['com']
-    return redirect("/show")	 
-
-@app.route('/show')
-def display():
-    return render_template("show.html")
-
+from flask_app import app
+from flask_app.controllers import dojos
 
 if __name__=="__main__":
-    app.run(debug=True,port=5004)
+    app.run(debug=True,port=5005)
