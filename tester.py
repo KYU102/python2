714624529be2eb8ea06e04ee89e84a279314954c
KYU102 <kevinyu25@gmail.com>
2022-07-27 14:37:29 -0700

    test

--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
 
+def test(str):
+    counter = {}
 
+    for word in str:
+        counter[word] = counter.get(word, 0) + 1
 
-x = 'Bana'
-
-print(x[0])
+    # for x, y in counter.items():
+    #     print( x,y)
+    return counter
+print(test(['a','b','c','a']))
 
 
